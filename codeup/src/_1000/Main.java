c255c1a1eaf3051cd271e7da217a6231f54aeaf8
xegenie <dltpwlswkdf@naver.com>
2024-08-28 00:22:17 +0900

    Main 코드업 1143할차례

---
+++ codeup/src/_1000/Main.java
@@ -0,0 +1,19 @@
+package _1000;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if (a==1 || b==1)
+			System.out.print("1");
+		else
+			System.out.print("0");
+	}
+}
