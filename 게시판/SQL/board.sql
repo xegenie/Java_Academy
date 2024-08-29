63e90418fa370d5960adeb3d40e5ee4ff37423e7
xegenie <dltpwlswkdf@naver.com>
2024-08-29 18:20:39 +0900

    17일 mysql

---
+++ 게시판/SQL/board.sql
@@ -0,0 +1,29 @@
+
+-- 게시판 테이블 생성
+CREATE TABLE `aloha`.`board` (
+  `no` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(100) NOT NULL,
+  `writer` VARCHAR(100) NOT NULL,
+  `content` TEXT NULL,
+  `reg_date` TIMESTAMP NOT NULL DEFAULT now(),
+  `upd_date` TIMESTAMP NOT NULL DEFAULT now(),
+  PRIMARY KEY (`no`))
+COMMENT = '게시판';
+
+-- 조회
+SELECT * FROM board;
+
+-- 추가
+INSERT INTO board (title, writer, content)
+VALUES ('게시글 제목01', '이세진', '집에가고싶다');
+
+-- 수정
+UPDATE board
+   SET title = '게시글 수정 제목 01'
+	, writer = '이현진'
+	, content = '내가먼저감'
+WHERE no = 1;
+
+-- 삭제
+DELETE FROM board
+WHERE no = 1;
