4750cc18dd8356cd38aa9b49fa4103d0fe35ecb6
xegenie <dltpwlswkdf@naver.com>
2024-09-02 09:32:49 +0900

    fx

--- JavaFx/src/application/Main.java
+++ JavaFx/src/application/Main.java
@@ -1,19 +1,28 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			// Main.fxml 파일을 로드하여 Parent 객체로 변환
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			
+			// Scene에 Parent 객체를 추가하고, Scene을 스테이지에 설정
+			Scene scene = new Scene(root);
+			
+			// CSS 파일 적용(필요시)
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			// 스테이지 설정 및 화면 출력
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("My JavaFX Application");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- JavaFx/src/module-info.java
+++ JavaFx/src/module-info.java
@@ -1,5 +1,8 @@
 module JavaFx {
 	requires javafx.controls;
+	requires javafx.fxml;
+	requires com.gluonhq.charm.glisten;
+	requires com.gluonhq.attach.util;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
